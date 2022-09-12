159a34a73920e04b8cbcf8d719212015b03e41e5
Dec <dec@Excel.local>
2022-09-12 23:25:00 +0100

    email-services

---
+++ src/controller/emailServices.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+import nodemailer from "nodemailer"
+import "dotenv/config"
+
+export async function sendEmail(req: Request, res: Response) {
+    const password = process.env.EMAIL_PASS
+    const { email, name, message } = req.body;
+   try{
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: 'pod.b.decagon@gmail.com',
+            pass: password
+        }
+    });
+    const mailOptions = {
+        from: 'pod.b.decagon@gmail.com',
+        to: 'ayodejisoyebo@gmail.com',
+        subject: "User Verification",
+        text: "Please confirm your email",
+        // html: `<p>${message}</p>`
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+        
+            res.status(400).json({
+            message: "An error occured",
+            err
+        })
+        } else {
+
+            res.status(200).json({
+            message: "Email sent successfully"
+            err
+        })
+        }
+    }
+    );
+    res.send("Email sent");
+   } catch(err){
+       res.status(500).json({
+              message: 'failed to send email',
+              route: "/confirmEmail"
+       })
+   }
+}
