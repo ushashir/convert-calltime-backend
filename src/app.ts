72ee6fb3e779938d19cb0ec032ab13ecfa434118
Decagon <decagon@Decagons-MacBook-Air-Usha.local>
2022-09-14 12:56:17 +0100

    cloudinary setup ready

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import createError from "http-errors";
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import bodyParser from "body-parser";
 
 import usersRouter from "./routes/userRoute";
 
@@ -9,34 +10,35 @@ const app = express();
 
 console.log("app running on port 3000");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.use("/api/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (
-	err: createError.HttpError,
-	req: Request,
-	res: Response,
-	next: NextFunction
+  err: createError.HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
-	next();
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+  next();
 });
 
 export default app;
