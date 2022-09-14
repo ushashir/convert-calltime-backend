64c00eb144a4382c6b65c637c5cdbe7916e76176
Dec <dec@GentleStorms-Air.local>
2022-09-14 11:18:34 +0100

    made the token usable only once

--- src/utils/emailService.ts
+++ src/utils/emailService.ts
@@ -8,9 +8,9 @@ dotenv.config();
 
 export async function emailServices(data: Record<string, unknown>, route: string) {
  
-	const {email, id, userName} = data;
+	const {email, id, userName, password} = data;
 
-	const token = jwt.sign({ user_id: id }, process.env.AUTH_SECRET as string, {expiresIn: "1h"});
+	const token = jwt.sign({ user_id: id }, process.env.AUTH_SECRET as string + password, {expiresIn: "1h"});
 
   const link = `${process.env.BASE_URL}/api/users/${route}/${token}`;
 
