91a0aec307a05a0f26c4db02687097ef1be2395c
Joseph Ero <erojoseph94@gmail.com>
2022-09-15 01:15:04 +0100

    Update src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ const app = express();
 
 console.log("app running on port 3000");
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use(express.json());
