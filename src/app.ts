b07f8ab9d759efc43a9a0c18c70ff9cff97941d1
Oforkaja Joseph <oforkajajoseph@gmail.com>
2022-09-27 23:12:07 +0100

    after I pulled

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,11 @@ console.log("app running on port 7000");
 app.use(cors());
 app.use(helmet());
 app.use(logger("dev"));
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
