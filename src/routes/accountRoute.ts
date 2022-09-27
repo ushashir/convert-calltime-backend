d3f40ad40d09c3064b9c9939f0a6e02ce667abfe
Decagon <decagon@Decagons-MacBook-Air-Usha.local>
2022-09-27 21:45:01 +0100

    fix signup headers bug

--- src/routes/accountRoute.ts
+++ src/routes/accountRoute.ts
@@ -6,24 +6,24 @@ import { auth } from "../utils/authMiddleware";
 const routes = Router();
 
 routes.post("/", auth, async (req: userRequest, res) => {
-  try {
-    const data = req.body;
-    const { user_id } = req.user;
-    const response = await createAccount(data, user_id);
-    res.status(201).json({ message: "Success", response });
-  } catch (error) {
-    res.status(400).json({ Error: error });
-  }
+	try {
+		const data = req.body;
+		const { user_id } = req.user;
+		const response = await createAccount(data, user_id);
+		return res.status(201).json({ message: "Success", response });
+	} catch (error) {
+		return res.status(400).json({ Error: error });
+	}
 });
 
 routes.get("/", auth, async (req: userRequest, res) => {
-  try {
-    const {user_id} =req.user
-    const response = await getAccounts(user_id);
-    res.status(200).json({message: "Success", response})
-  } catch (error) {
-    res.status(400).json({Error: error})
-  }
+	try {
+		const {user_id} =req.user
+		const response = await getAccounts(user_id);
+		return res.status(200).json({message: "Success", response})
+	} catch (error) {
+		return res.status(400).json({Error: error})
+	}
 })
 
 export default routes;
