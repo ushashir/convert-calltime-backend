e85b05ae2b79eb4feeb0f707ed973933f58ac042
Dec <dec@GentleStorms-Air.local>
2022-10-03 07:11:45 +0100

    update on txRecord model

--- src/controller/txController.ts
+++ src/controller/txController.ts
@@ -1,5 +1,6 @@
 import prisma from "../utils/prismaClient";
 import { txRecordSchema } from "../utils/validation";
+import nodemailer from "nodemailer"
 
 export async function recordTx(txData: Record<string, unknown>, id: string) {
     
@@ -7,15 +8,35 @@ export async function recordTx(txData: Record<string, unknown>, id: string) {
     if (!validData.success) throw validData.error
     const {amount, network, phone} = validData.data
     const amountAfterRates = Number(amount) * 0.7
+
+    //get user email
+    const user = await prisma.user.findUnique({where:{id}})
+    if(!user) throw "User record not found"
     const response = await prisma.txRecord.create({
         data: {
-            amount: amountAfterRates,
+            amount: Number(amount),
+            amountToReceive: amountAfterRates,
             network,
             phone,
-            userId:id
+            userId: id,
+            email: user?.email
         }
     })
-    return response
 
-    
+    const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS
+		},
+	});
+
+     // send mail with defined transport object
+ await transporter.sendMail({
+    from: 'Airtime2Cash', // sender address
+    to: "erojoseph94@gmail.com", // list of receivers
+    subject: "Airtime Transfer Notification", // Subject line
+    html: "<b>Hello Admin, <br/> There is a new transaction</b>", // html body
+  });
+    return response
 }
